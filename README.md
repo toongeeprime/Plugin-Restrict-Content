ae32957f2a73e3bcd472bc41cc203a7d027e724d
Tunji Makanjuola <toongeeprime@gmail.com>
2022-02-27 23:56:01 +0100

    Vital edits
    Changed plugin URL, added comments, improved README.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Plugin-Restrict-Content
- WordPress Shortcodes for restricting contents
+# WP Restrict Content Shortcodes
+ WordPress Basic Shortcodes for restricting contents from either logged in or logged out users
+
+### Shortcodes Usage
+ Wrap the shortcodes around the contents you want to restrict...
+
+ Use shortcode \[showto_loggedin_users\] **Your contents for logged in users go here** \[\/showto_loggedin_users\]
+ Use shortcode \[showto_loggedout_users\] **Your contents for logged out visitors go here** \[\/showto_loggedout_users\]
--- toongeeprime-restrict-content/toongeeprime-restrict-content.php
+++ toongeeprime-restrict-content/toongeeprime-restrict-content.php
@@ -4,16 +4,16 @@
  * Basic content restriction shortcodes plugin for WordPress
  *
  * @package		toongeeprime-restrict-content
- * @link		https://github.com/toongeeprime/Plugin-Restrict-Content
+ * @link		https://github.com/toongeeprime/wp-restrict-content-shortcodes
  * @author		ToongeePrime <toongeeprime@gmail.com>
- * @copyright	2022
+ * @copyright		2022
  * @license		GPL v2 or later
  *
  * Plugin Name:		ToongeePrime Restrict Content
  * Description:		Shortcodes to Restrict contents based on whether users are logged in or logged out
- * Version:			1.0
- * Plugin URI:		https://github.com/toongeeprime/Plugin-Restrict-Content
- * Author:			ToongeePrime
+ * Version:		1.0
+ * Plugin URI:		https://github.com/toongeeprime/wp-restrict-content-shortcodes
+ * Author:		ToongeePrime
  * Author URI:		https://github.com/toongeeprime/
  * Text Domain:		toongeeprime-restrict-content
  * Domain Path:		/languages/
@@ -39,6 +39,7 @@
  */
 add_shortcode( 'showto_loggedin_users', 'toongeeprime_showTo_loggedIn' );
 function toongeeprime_showTo_loggedIn( $atts, $content, $tag ) {
+	// check that user is logged in
 	if ( is_user_logged_in() ) {
 		$content	=	do_shortcode( $content );
 		return '<div class="logged-in-content">' . $content . '</div>';
@@ -51,6 +52,7 @@ function toongeeprime_showTo_loggedIn( $atts, $content, $tag ) {
  */
 add_shortcode( 'showto_loggedout_users', 'toongeeprime_showTo_loggedOut' );
 function toongeeprime_showTo_loggedOut( $atts, $content, $tag ) {
+	// check that user is Not logged in
 	if ( ! is_user_logged_in() ) {
 		$content = do_shortcode( $content );
 		return '<div class="logged-out-content">' . $content . '</div>';
